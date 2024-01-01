06a9d4ac09e146287ec522e73a72c0ba582121e6
PHX Digital Media <72950126+xpsnetworks@users.noreply.github.com>
2024-01-01 14:39:19 -0300

    Update next.config.mjs
    Allow public embed from viewer

--- apps/builder/next.config.mjs
+++ apps/builder/next.config.mjs
@@ -58,19 +58,6 @@ const nextConfig = {
     config.resolve.alias['openai'] = false
     return config
   },
-  headers: async () => {
-    return [
-      {
-        source: '/(.*)?',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-        ],
-      },
-    ]
-  },
   async rewrites() {
     return process.env.NEXT_PUBLIC_POSTHOG_KEY
       ? [
